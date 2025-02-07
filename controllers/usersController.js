77c152f97f8e83dde7e898cb59f9501307c58cd8
Benas <benas.ivanauskas@gmail.com>
2025-02-07 10:55:45 +0200

    GYM-TRACK forgot-password and reset-password endpoints

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,12 +1,22 @@
 import { v4 as uuidv4 } from "uuid";
+import JWT from "jsonwebtoken";
 import {
   comparePassword,
   currentDate,
   hashedPassword,
+  isPassStrong,
   signToken,
+  verifyToken,
 } from "../utils/helpers.js";
-import { getUserByEmail, insertUser } from "../services/userService.js";
+import {
+  getUserByEmail,
+  insertUser,
+  removeExpToken,
+  storeResetToken,
+  updateUserPassword,
+} from "../services/userService.js";
 import ErrorHandler from "../errors/error.js";
+import { sendPasswordResetEmail } from "../mailer.js";
 
 export const createUser = async (req, res, next) => {
   try {
@@ -21,7 +31,7 @@ export const createUser = async (req, res, next) => {
     }
     const id = uuidv4();
     const hashPassword = await hashedPassword(password);
-    const token = signToken(email, id);
+    const token = signToken(email, id, "1h");
     const currentTimeStamp = currentDate();
 
     await insertUser(id, name, email, hashPassword, currentTimeStamp);
@@ -55,12 +65,107 @@ export const loginUser = async (req, res, next) => {
     if (!isMatch) {
       throw new ErrorHandler(401, "Invalid email or password");
     }
-    const token = signToken(user.email, user.id);
+    const token = signToken(user.email, user.id, "1h");
 
     res.status(200).json({
       message: "Login successful",
       token,
     });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const forgotPassword = async (req, res, next) => {
+  try {
+    const { email } = req.body;
+    if (!email) {
+      throw new ErrorHandler(400, "Email fields is empty. Please fill field");
+    }
+
+    const user = await getUserByEmail(email);
+    if (!user) {
+      throw new ErrorHandler(
+        404,
+        "Email does not exist. Please provide correct email adress"
+      );
+    }
+    const { id } = user;
+    const tempToken = signToken(email, id, "5m");
+    await removeExpToken(id);
+
+    const expirationTime = new Date();
+    expirationTime.setMinutes(expirationTime.getMinutes() + 5);
+
+    await storeResetToken(id, tempToken, expirationTime);
+    const resetLink = `${process.env.FRONTED_URL}/v1/api/reset-password?token=${tempToken}`;
+
+    try {
+      await sendPasswordResetEmail(user.email, resetLink);
+      res
+        .status(200)
+        .json({ message: "Password reset link has been sent to your email" });
+    } catch (emailErr) {
+      throw new ErrorHandler(
+        500,
+        "Failed to send reset email. Please try again later."
+      );
+    }
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const resetPassword = async (req, res, next) => {
+  try {
+    const { token } = req.query;
+    const { password } = req.body;
+
+    if (!token || !password) {
+      throw new ErrorHandler(400, "Token are required.");
+    }
+    if (!password) {
+      throw new ErrorHandler(
+        400,
+        "Password field cannot be empty. Please provide password"
+      );
+    }
+    const checkingPassword = isPassStrong(password);
+    if (!checkingPassword) {
+      throw new ErrorHandler(
+        400,
+        "Password is not strong enough. Please write stronger password"
+      );
+    }
+    const decoded = verifyToken(token, process.env.SECRET_KEY);
+    const { email, id } = decoded;
+
+    const user = await getUserByEmail(email);
+    if (!user || user.id !== id) {
+      throw new ErrorHandler(401, "Invalid token or user not found.");
+    }
+
+    const hashPassword = await hashedPassword(password);
+    const currentTimeStamp = currentDate();
+    await updateUserPassword(id, email, hashPassword, currentTimeStamp);
+
+    res.status(200).json({
+      message: `User with ${email} was successfully updated password`,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const getResetLink = (req, res, next) => {
+  const { token } = req.query;
+  if (!token) {
+    throw new ErrorHandler(400, "Invalid or missing token");
+  }
+  try {
+    return res
+      .status(200)
+      .send(`This is the route where user need to change password`);
   } catch (err) {
     next(err);
   }
--- services/userService.js
+++ services/userService.js
@@ -1,4 +1,6 @@
 import { knex, db } from "../models/database.js";
+import cron from "node-cron";
+
 export const getUserByEmail = (email) => {
   return db.oneOrNone(
     knex("users").select("*").andWhere({ email: email }).limit(1).toQuery()
@@ -15,6 +17,72 @@ export const insertUser = (id, name, email, hashPassword, currentDate) => {
         password: hashPassword,
         created_at: currentDate,
         updated_at: currentDate,
+      })
+      .toQuery()
+  );
+};
+
+export const removeExpToken = (id) => {
+  return db.none(
+    knex("users")
+      .where({ id })
+      .update({
+        reset_password_token: null,
+        reset_password_expires: null,
+        is_resetting_password: false,
+      })
+      .toQuery()
+  );
+};
+
+export const removeExpiredTokens = async () => {
+  console.log("Cleaning up expired reset tokens...");
+  const now = new Date();
+  await db.oneOrNone(
+    knex("users")
+      .where("reset_password_expires", "<", now)
+      .update({
+        reset_password_token: null,
+        reset_password_expires: null,
+        is_resetting_password: false,
+      })
+      .toQuery()
+  );
+  console.log("Expired tokens cleaned up!");
+};
+
+cron.schedule("*/6 * * * *", async () => {
+  await removeExpiredTokens();
+});
+
+export const storeResetToken = (id, token, expTime) => {
+  return db.oneOrNone(
+    knex("users")
+      .andWhere({ id })
+      .update({
+        reset_password_token: token,
+        reset_password_expires: expTime,
+        is_resetting_password: true,
+      })
+      .toQuery()
+  );
+};
+
+export const updateUserPassword = (
+  id,
+  email,
+  hashPassword,
+  currentTimeStamp
+) => {
+  return db.none(
+    knex("users")
+      .andWhere({ id: id, email: email })
+      .update({
+        password: hashPassword,
+        updated_at: currentTimeStamp,
+        reset_password_token: null,
+        reset_password_expires: null,
+        is_resetting_password: false,
       })
       .toQuery()
   );
